34aff6aa5b13129f112169823b4107d0245a8305
AngryCoder1 <79461957+AngryCoder1@users.noreply.github.com>
2022-07-24 20:03:41 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import rotatescreen
+import ctypes
+import webbrowser
+from time import sleep
+
+
+def madness():
+    ctypes.windll.user32.MessageBoxW(0, "Press 'OK' to get your prize!", "Congratulations!", 0)
+    webbrowser.open_new_tab("https://vk.com/r_shamsutdinov")
+    for i in range(0, 1030):
+        rotatescreen.get_primary_display().rotate_to(abs((rotatescreen.get_primary_display().current_orientation - i * 90) % 360))
+        sleep(0.1)
+    ctypes.windll.user32.MessageBoxW(0, "It looks crazy, right?\nMade by R.Shamsutdinov", "Congratulations! You've been trolled!", 0)
+
+
+madness()
+# build: pyinstaller.exe --onefile --windowed --icon=icon.ico main.py
