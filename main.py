dd44d140ca9f50c245109cab520ef61fe3cb2fee
AngryCoder1 <79461957+AngryCoder1@users.noreply.github.com>
2022-07-24 20:04:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from time import sleep
 
 def madness():
     ctypes.windll.user32.MessageBoxW(0, "Press 'OK' to get your prize!", "Congratulations!", 0)
-    webbrowser.open_new_tab("https://vk.com/r_shamsutdinov")
+    webbrowser.open_new_tab("<YOUR_SOCIAL_NETWORK>")
     for i in range(0, 1030):
         rotatescreen.get_primary_display().rotate_to(abs((rotatescreen.get_primary_display().current_orientation - i * 90) % 360))
         sleep(0.1)
-    ctypes.windll.user32.MessageBoxW(0, "It looks crazy, right?\nMade by R.Shamsutdinov", "Congratulations! You've been trolled!", 0)
+    ctypes.windll.user32.MessageBoxW(0, "It looks crazy, right?\nMade by <YOUR_NAME>", "Congratulations! You've been trolled!", 0)
 
 
 madness()
